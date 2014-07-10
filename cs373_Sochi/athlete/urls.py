834cf7d7fa730d5ef9bb698be072c52ea85f269c
Austin Hooper <ajhooper@cs.utexas.edu>
2014-07-09 21:24:29 -0500

    Working on hooking static pages to Django

--- cs373_Sochi/athlete/urls.py
+++ cs373_Sochi/athlete/urls.py
@@ -4,5 +4,7 @@ from athlete import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<athlete_id>\d+)/$', views.detail, name='detail'),
+    url(r'^ireenwust/', views.ireen, name='ireen'),
+    url(r'^tobiasarlt/', views.tobias, name='tobias'),
+    #url(r'^(?P<athlete_id>\d+)/$', views.detail, name='detail'),
 )
--- cs373_Sochi/athlete/views.py
+++ cs373_Sochi/athlete/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def index(request):
     return HttpResponse("Put list of athletes here or something")
 
 def detail(request, athlete_id):
     return HttpResponse("You're looking at athlete %s." % athlete_id)
+
+def ireen(request):
+    return render_to_response('athlete/ireenwust.html')
+
+def tobias(request):
+    return render_to_response('athlete/tobiasarlt.html')
