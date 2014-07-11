a38dfffd56bd657cb59ebbabed9ae77b03f0b35c
Austin Hooper <ajhooper@cs.utexas.edu>
2014-07-10 21:07:32 -0500

    setting django up for pythonanywhere push

--- cs373_Sochi/cs373_Sochi/urls.py
+++ cs373_Sochi/cs373_Sochi/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import RedirectView
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'cs373_Sochi.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', RedirectView.as_view(url='/home/')),
     url(r'^home/', include('home.urls')),
     url(r'^athlete/', include('athlete.urls')),
     url(r'^country/', include('country.urls')),
