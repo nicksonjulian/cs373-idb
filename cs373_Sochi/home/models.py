a5ce498a1fba9b06a0aac7de8563f7bf2e2d0be0
ajhooper <ajhooper654@gmail.com>
2014-07-22 01:09:17 +0000

    Made it so models are now sorted when viewed in admin

--- cs373_Sochi/home/models.py
+++ cs373_Sochi/home/models.py
@@ -21,6 +21,9 @@ class Athlete(models.Model):
     silver_medals = models.ManyToManyField('Events', blank=True, related_name='athlete_silver')
     bronze_medals = models.ManyToManyField('Events', blank=True, related_name='athlete_bronze')
 
+    class Meta:
+        ordering = ['last_name']
+
     def __str__(self):
         return self.first_name + " "  + self.last_name
 
@@ -31,6 +34,9 @@ class Events(models.Model):
     gold_medalists =  models.ManyToManyField(Athlete, blank=True, related_name='events_gold')
     silver_medalists =  models.ManyToManyField(Athlete, blank=True, related_name='events_silver')
     bronze_medalists =  models.ManyToManyField(Athlete, blank=True, related_name='events_bronze')
+
+    class Meta:
+        ordering = ['sport','name']
 
     def __str__(self):
         return self.sport + ": " + self.name
@@ -47,7 +53,9 @@ class Country(models.Model):
     coody = models.FloatField(blank=True, null=True)
     coodz = models.IntegerField(blank=True, null=True)
 
-    #For Events with medals, load dynamically (?)
+    class Meta:
+        ordering = ['name']
 
+    #For Events with medals, load dynamically (?)
     def __str__(self):
         return self.name
