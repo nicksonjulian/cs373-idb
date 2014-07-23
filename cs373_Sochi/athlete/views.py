de165604f377c488d72d693f989093bcc9cd9616
ajhooper <ajhooper654@gmail.com>
2014-07-23 00:44:19 +0000

    Working on Athlete dynamic page

--- cs373_Sochi/athlete/views.py
+++ cs373_Sochi/athlete/views.py
@@ -9,15 +9,15 @@ def index(request):
 def detail(request, athlete_id):
     if (athlete_id == "0"):
         athlete = Athlete.objects.get(first_name="Ireen")
-        context = {'athlete': athlete}
+        context = {'athlete': athlete, 'picture': "/static/athlete/images/ireenwust.jpg", 'flag': "/static/athlete/images/ned.png", 'video': "https://www.youtube.com/v/50H_s-89ckU"}
         return render(request, 'athlete/dynamic_athlete.html', context)
     elif (athlete_id == "1"):
         athlete = Athlete.objects.get(first_name="Meryl")
-        context = {'athlete': athlete}
+        context = {'athlete': athlete, 'picture': "/static/athlete/images/meryldavis.png", 'flag': "/static/athlete/images/usa.png", 'video': "https://www.youtube.com/v/w9yX3S9VdtI"}
         return render(request, 'athlete/dynamic_athlete.html', context)
     elif (athlete_id == "2"):
         athlete = Athlete.objects.get(first_name="Tobias")
-        context = {'athlete': athlete}
+        context = {'athlete': athlete, 'picture': "/static/athlete/images/tobiasarlt.png", 'flag': "/static/athlete/images/ger.png", 'video': "https://www.youtube.com/v/zpzMtBK-yOA"}
         return render(request, 'athlete/dynamic_athlete.html', context)
     else:
         return HttpResponse("You're looking at athlete %s." % athlete_id)
