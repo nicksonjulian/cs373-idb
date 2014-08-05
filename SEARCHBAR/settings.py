60293f80bf7493fbbfe2cdbd4171aa454f8ccb72
atlus7 <nicksonjulian@yahoo.com.sg>
2014-08-04 21:40:17 -0500

    added haystack+whoosh, i cant figure out some things

---
+++ SEARCHBAR/settings.py
@@ -0,0 +1,110 @@
+"""
+Django settings for cs373_Sochi project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '4jw4lk!dkx+oh6j$hg8!lq34+x4g&qyv8dxnu&eyqj-n(u&7c&'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'home',
+    'athlete',
+    'country',
+    'event',
+    'api',
+    'south',
+    'rest_framework',
+    'whoosh',
+    'haystack'
+)
+
+WHOOSH_INDEX = os.path.join(PROJECT_DIRECTORY, 'whoosh/')
+
+HAYSTACK_CONNECTIONS = {
+    'default':{
+        'ENGINE': 'haystack.backends.whoosh-backend.WhooshEngine',
+        'PATH': WHOOSH_INDEX,
+    },
+}
+
+HAYSTACK_SEARCH_ENGINE = 'whoosh'
+
+HAYSTACK_WHOOSH_PATH = '/path/to/server/writable/directory'
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'cs373_Sochi.urls'
+
+WSGI_APPLICATION = 'cs373_Sochi.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'ajhooper$cs373-Sochi',
+        'USER': 'ajhooper',
+        'PASSWORD': 'bumbershoot',
+        'HOST': 'mysql.server',
+    }
+}
+
+SOUTH_DATABASE_ADAPTERS = { 'default' : 'south.db.mysql' }
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_ROOT = '/home/ajhooper/cs373-idb/cs373_Sochi/static/'
+STATIC_URL = '/static/'
