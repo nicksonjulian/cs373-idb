0b454ad293ae2c8a6b8c0fbd935ef4e80543af72
nicksonjulian <nicksonjulian@utexas.edu>
2014-08-07 14:25:21 -0500

    Update exercisingAPI.py

--- exercisingAPI.py
+++ exercisingAPI.py
@@ -7,10 +7,16 @@ def exerciseapi(theid):
     data = json.loads(html.decode('utf8'))
     title = data['title'] #book's title 
     authorid = data['author']
+    publisherid = data['publishers']
 
     response2 = urllib.request.urlopen('http://alabibliotheque.pythonanywhere.com/authors/' +  str(authorid) + '.json')
     html2 = response2.read()
     data2 = json.loads(html2.decode('utf8'))
     authorfullname = data2['name'] #books's author
+    
+    response3 = urllib.request.urlopen('http://alabibliotheque.pythonanywhere.com/publishers/' +  str(publisherid) + '.json')
+    html3 = response3.read()
+    data3 = json.loads(html3.decode('utf8'))
+    publishername = data3['name'] #books's author
 
-    #print(authorfullname + "   " + title) #test
+    #print(authorfullname + "   " + title + " " + publishername) #test
