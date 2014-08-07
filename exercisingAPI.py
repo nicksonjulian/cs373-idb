81c0c52d5f523d16bcf26fcce555559dfa44b590
nicksonjulian <nicksonjulian@utexas.edu>
2014-08-07 14:43:09 -0500

    Update exercisingAPI.py

--- exercisingAPI.py
+++ exercisingAPI.py
@@ -1,22 +1,25 @@
 import urllib.request
 import json
 
-def exerciseapi(bookid, title, authorfullname, publishername):
-    response = urllib.request.urlopen('http://alabibliotheque.pythonanywhere.com/books/' + str(bookid) + '.json')
+def exerciseapi(theid, a):
+    response = urllib.request.urlopen('http://alabibliotheque.pythonanywhere.com/books/' + str(theid) + '.json')
     html = response.read()
     data = json.loads(html.decode('utf8'))
     title = data['title'] #book's title 
     authorid = data['author']
-    publisherid = data['publishers']
+    publisherids = data['publishers']
+    a.append(title)
 
     response2 = urllib.request.urlopen('http://alabibliotheque.pythonanywhere.com/authors/' +  str(authorid) + '.json')
     html2 = response2.read()
     data2 = json.loads(html2.decode('utf8'))
     authorfullname = data2['name'] #books's author
-    
-    response3 = urllib.request.urlopen('http://alabibliotheque.pythonanywhere.com/publishers/' +  str(publisherid) + '.json')
-    html3 = response3.read()
-    data3 = json.loads(html3.decode('utf8'))
-    publishername = data3['name'] #books's author
+    a.append(authorfullname)
 
-    #print(authorfullname + "   " + title + " " + publishername) #test
+
+    for publisherid in publisherids:
+        response3 = urllib.request.urlopen('http://alabibliotheque.pythonanywhere.com/publishers/' +  str(publisherid) + '.json')
+        html3 = response3.read()
+        data3 = json.loads(html3.decode('utf8'))
+        publishername = data3['name'] #books's publisher
+        a.append(publishername)
