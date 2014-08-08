ae0729d7e9c4e09fa2fee774c158434e12334b35
ajhooper <ajhooper654@gmail.com>
2014-08-08 18:21:31 +0000

    Updated tests closes #41

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from home.models import Athlete, Country, Events
 
 class HomeTests(TestCase):
+    fixtures = ['testdata.json']
 
     def test_Athlete1(self):
         athlete = Athlete(self,"John", "Doe", "US")
@@ -18,6 +19,14 @@ class HomeTests(TestCase):
         athlete = Athlete(self, "John", "Doe", country.id)
         self.assertEqual(athlete.country.name, "Egypt")
 
+    def test_Athlete_type(self):
+        athlete = Athlete(self, "John", "Doe", "US")
+        self.assertEqual(athlete.get_cname(), "athlete")
+
+    def test_Athlete_database(self):
+        athlete = Athlete.objects.get(first_name="Ireen")
+        self.assertEqual(athlete.last_name, "Wust")
+
     def test_Events1(self):
         event = Events(self, "Luge Doubles", "Luge", "Riding on cars")
         self.assertEqual(event.name, "Luge Doubles")
@@ -29,6 +38,14 @@ class HomeTests(TestCase):
     def test_Events3(self):
         event = Events(self, "Luge Doubles", "Luge", "Riding on cars")
         self.assertEqual(event.desc, "Riding on cars")
+
+    def test_Events_type(self):
+        event = Events(self, "Luge Doubles", "Luge", "Riding on cars")
+        self.assertEqual(event.get_cname(), "events")
+
+    def test_Events_database(self):
+        event = Events.objects.get(name="Luge Doubles")
+        self.assertEqual(event.sport, "Luge")
 
     def test_Country1(self):
         country = Country(self, "Egypt", "Made of sand")
@@ -45,3 +62,16 @@ class HomeTests(TestCase):
     def test_Country4(self):
         country = Country(self, "Egypt", "Made of sand", 10, 2, 8)
         self.assertEqual(country.total_bronze_medals, 8)
+
+    def test_Country_type(self):
+        country = Country(self, "Egypt", "Made of sand")
+        self.assertEqual(country.get_cname(), "country")
+
+    def test_Country_database(self):
+        country = Country.objects.get(name="Germany")
+        self.assertEqual(country.country_code, "ger")
+
+    def test_Athlete_Country_link(self):
+        athlete = Athlete.objects.get(first_name="Ireen")
+        country = athlete.country
+        self.assertEqual(country.name, "Netherlands")
